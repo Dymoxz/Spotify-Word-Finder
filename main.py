597890040b43ea27ab1909fa4d483d050cf85af9
Dymo Waltheer <82333980+Dymoxz@users.noreply.github.com>
2021-06-06 17:29:14 +0200

    Better Search options

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import sys
+from colorama import init
+init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
+from termcolor import cprint
+from pyfiglet import figlet_format
 
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="8e69f7d8ddfa45f1888b56411e0ac440",
                                                            client_secret="0f742321201f4e5996021d59d2608922"))
-
-print('SPOTIPY ANIMAL FINDER')
+cprint(figlet_format('Spotipy \n Animal \n Finder', font='doom'),
+       'yellow', 'on_red', attrs=['bold'])
 print('')
 print('')
 
@@ -39,12 +44,14 @@ while True:
 
     searchBy = input('Do you want to search by (1/2): \n 1. Genre \n 2. Artist \n')
 
+    amount = input('How much song do you want to search through (max = 1000): \n')
 
     if searchBy == '2':
         offsetA1 = 0
         repeatA1 = 0
+        amountOfRepeats = int(amount) / 50 - 1
         artistInput = input('Enter artist: ')
-        while repeatA1 < 19:
+        while repeatA1 < amountOfRepeats:
             artist1(offset=offsetA1 + 50, artist=artistInput)
             offsetA1 += 50
             repeatA1 += 1
@@ -53,10 +60,15 @@ while True:
         offsetG1 = 0
         repeatG1 = 0
         genreInput = input('Enter genre: ')
-        while repeatG1 < 19:
+        while repeatG1 < amountOfRepeats:
             genre1(offset=offsetG1 + 50, genre=genreInput)
             offsetG1 += 50
             repeatG1 += 1
+    else:
+        print('Invalid Input')
+        continue
+
+
 
     while True:
         answer = str(input('Restart? (y/n): '))
